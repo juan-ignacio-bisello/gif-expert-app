287a53a819af71583ad9609704339c8aaa56a62c
juanib99 <juanignaciobisello@hotmail.com>
2024-03-25 23:39:03 -0300

    +

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,6 +1,20 @@
+import { useState } from "react"
 
 export const GifExpertApp = () => {
+
+  const [ categorias , setCategorias ] = useState([ 'One Punch', 'Dragon Ball' ]);
+
+  console.log(categorias)
   return (
-    <h1>GifExpertApp</h1>
+    <>
+     <h1>GifExpertApp</h1>
+
+     <ol>
+       { categorias.map( category => {
+          return <li key={ category }>{ categorias }</li>
+       } ) }
+     </ol>
+    </>
+    
   )
 }
