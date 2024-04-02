8f5c0dd089499caba4539b0e7e862667bdacd6ac
juanib99 <juanignaciobisello@hotmail.com>
2024-04-02 14:13:53 -0300

    final gif-expert-app

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,22 +1,28 @@
-import { useEffect, useState } from "react";
-import { getGifs } from "../helper/getGifs";
+import { GifItem } from "./GifItem";
+import { useFetchGifs } from "../hooks/useFetchGifs";
 
 export const GifGrid = ( { category } ) => {
 
-    const [counter, setCounter] = useState(10);
-
-    useEffect( () => {
-        getGifs( category );
-    }, [ ] )
-
-    
+  const { images, isLoading } = useFetchGifs( category );
 
   return (
     <>
         <h3>{ category }</h3>
+        {
+          isLoading && ( <h2>Cargando...</h2> )
+        }
+        
 
-        <h5>{ counter}</h5>
-        <button onClick={ () => setCounter( counter + 1 )}>+1</button>
+        <div className="card-grid" >
+          {
+            images.map( ( image ) => (
+              <GifItem 
+              key={ image.id }
+              { ...image }
+              />
+            ))
+          }
+        </div>
     </>
   )
 }
--- src/helper/getGifs.js
+++ src/helper/getGifs.js
@@ -1,6 +1,6 @@
 export const getGifs = async( category ) => {
     
-    const url = `https://api.giphy.com/v1/gifs/search?api_key=cek5gDZM1f1ntZrLpNih26RKINwRBi1M&q=${ category }&limit=20`;
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=cek5gDZM1f1ntZrLpNih26RKINwRBi1M&q=${ category }&limit=10`;
     const resp = await fetch( url );
     const { data }= await resp.json();
 
@@ -10,6 +10,5 @@ export const getGifs = async( category ) => {
         url: img.images.downsized_medium.url
     }));
 
-    console.log(gifs)
     return gifs;
 }
