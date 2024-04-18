8e73ca35f920eb8a2d30999905e36b8ab8d21634
juanib99 <juanignaciobisello@hotmail.com>
2024-04-18 13:01:36 -0300

    unit test completado

--- test/components/AddCategory.test.jsx
+++ test/components/AddCategory.test.jsx
@@ -13,4 +13,37 @@ describe('Puebas en <AddCategory/>', () => {
         expect( input.value ).toBe('Saitama');
         // screen.debug()
      });
+
+     test('debe llamar onNewCategory si el input tiene un valor', () => { 
+        
+        const inputValue = 'Saitama';
+        
+        const onNewCategory = jest.fn();
+
+        render( <AddCategory onNewCategory={ onNewCategory } /> );
+        
+        const input = screen.getByRole('textbox');
+        const form = screen.getByRole('form');
+
+        fireEvent.input( input, { target: { value: inputValue }} );
+        fireEvent.submit( form );
+        // screen.debug();
+
+        expect( input.value ).toBe('');   
+        
+        expect( onNewCategory ).toHaveBeenCalled();
+        expect( onNewCategory ).toHaveBeenCalledTimes( 1 );
+        expect( onNewCategory ).toHaveBeenCalledWith( inputValue );
+      });
+
+      test('no debe llamar el onNewCategory si el input está vacio', () => { 
+                
+        const onNewCategory = jest.fn();
+        render( <AddCategory onNewCategory={ onNewCategory } /> );
+        
+        const form = screen.getByRole('form');
+        fireEvent.submit( form );
+        
+        expect( onNewCategory ).not.toHaveBeenCalled();
+        })
 });
